ff186f35fb799378bc0f3b5b2ea3e31934403826
Mecca Jay <mecca.nicolej@gmail.com>
2020-01-30 03:28:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,12 @@
 
 def square_array (array)
-  numbers = [1,2,3]
+  numbers = ["1,2,3"]
   array.each do 
   square_array(numbers)  
 end  
 
 
 
- #it 'does not call on collect/map or inject' do
     #numbers = [1,2,3]
    #expect(numbers).to_not receive(:collect)
     #expect(numbers).to_not receive(:map)
