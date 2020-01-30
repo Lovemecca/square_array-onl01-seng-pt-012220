c1ba3373a19b6a5ed0afd9c234bb67bd63e27d75
Mecca Jay <mecca.nicolej@gmail.com>
2020-01-30 03:30:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,12 +7,5 @@ end
 
 
 
-    #numbers = [1,2,3]
-   #expect(numbers).to_not receive(:collect)
-    #expect(numbers).to_not receive(:map)
-   #expect(numbers).to_not receive(:inject)
-    #square_array(numbers)
-  #end
-## Objectives
-## Instructions
+    
 
